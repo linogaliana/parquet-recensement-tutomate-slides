32868ba6451256a54b1d43f7c0428a39e3acdb45
lgaliana <lino.galiana@insee.fr>
2024-11-02 16:42:32 +0000

    Add r init script

---
+++ sspcloud/init-r.sh
@@ -0,0 +1,30 @@
+REPO_URL=https://github.com/linogaliana/parquet-recensement-tutomate.git
+WORK_DIR=/home/onyxia/work
+FORMATION_DIR=${WORK_DIR}/parquet-recensement-tutomate
+QUARTO_FILE=tuto/r.qmd
+
+# Clone the repository
+git clone $REPO_URL $FORMATION_DIR
+chown -R onyxia:users $FORMATION_DIR
+
+# Install dependencies
+install2.r here
+
+# Open the project
+echo \
+"
+setHook('rstudio.sessionInit', function(newSession) {
+ if (newSession)
+  {
+    rstudioapi::navigateToFile('tp/${QUARTO_FILE}')
+  }
+}, action = 'append')
+
+setHook('rstudio.sessionInit', function(newSession) {
+  if (newSession && identical(getwd(), '${WORK_DIR}'))
+  {
+    message('Activation du projet RStudio')
+    rstudioapi::openProject('${FORMATION_DIR}')
+  }
+}, action = 'append')
+" >> /home/onyxia/.Rprofile
