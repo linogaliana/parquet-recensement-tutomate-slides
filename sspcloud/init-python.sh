5808ff656ecb4f22db12d678b0131e978dca02c4
lgaliana <lino.galiana@insee.fr>
2024-11-05 09:16:09 +0000

    update

--- sspcloud/init-python.sh
+++ sspcloud/init-python.sh
@@ -1,8 +1,8 @@
+WORK_DIR="/home/onyxia/work"
+DOWNLOAD_URL="https://linogaliana.github.io/parquet-recensement-tutomate/tuto/python.ipynb"
+CHAPTER="tuto-parquet-python"
+curl -L $DOWNLOAD_URL -o "${WORK_DIR}/${CHAPTER}.ipynb"
+
 git clone --depth 1 https://github.com/linogaliana/parquet-recensement-tutomate.git tuto
 pip install -r tuto/requirements.txt
 rm -rf tuto
-
-WORK_DIR="/home/onyxia/work"
-DOWNLOAD_URL="https://linogaliana.github.io/parquet-recensement-tutomate/tuto/python.ipynb"
-CHAPTER="tuto-parquet-python.ipynb"
-curl -L $DOWNLOAD_URL -o "${WORK_DIR}/${CHAPTER}.ipynb"
