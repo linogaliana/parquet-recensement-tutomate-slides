2208dd834043cc9f9728aaa0f85bec7bd18cb9c4
lgaliana <lino.galiana@insee.fr>
2024-11-05 09:39:33 +0000

    clean init script

--- sspcloud/init-python.sh
+++ sspcloud/init-python.sh
@@ -7,7 +7,7 @@ CHAPTER="tuto-parquet-python"
 
 git clone --depth 1 https://github.com/linogaliana/parquet-recensement-tutomate.git ${DESTINATION_GIT}
 pip install -r "${DESTINATION_GIT}/requirements.txt"
-# rm -rf tuto
+rm -rf "${DESTINATION_GIT}"
 
 echo $DOWNLOAD_URL
 curl -L $DOWNLOAD_URL -o "${WORK_DIR}/${CHAPTER}.ipynb"
