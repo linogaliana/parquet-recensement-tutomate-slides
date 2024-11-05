dec0690b5af51532507872ca46931507db200d07
lgaliana <lino.galiana@insee.fr>
2024-11-05 08:59:30 +0000

    Ajout init script python

---
+++ sspcloud/init-python.sh
@@ -0,0 +1,5 @@
+git clone --depth 1 https://github.com/linogaliana/parquet-recensement-tutomate.git tuto
+pip install -r tuto/requirements.txt
+rm -rf tuto
+
+curl -O https://linogaliana.github.io/parquet-recensement-tutomate/tuto/python.ipynb
